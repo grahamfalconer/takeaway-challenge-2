af3fae06676aad6c849b9838d9bc6996fa206b42
Graham <grahamfalconer@icloud.com>
2020-06-16 17:17:04 +0100

    Tests added for menu dish selection

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -4,7 +4,6 @@ describe Basket do
   it 'can recieve items' do
     basket = Basket.new
     item = ["The Prawn Nigiri",  3]
-    
     basket.items << item
     expect(basket.items.length).to eq 1
   end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,10 +1,23 @@
 require_relative '../src/menu.rb'
 
-describe Menu do
+describe 'Menu Display' do
   it 'can show a list of dishes and prices' do
     menu = Menu.new
     expect(menu.show_menu).to be_a_kind_of(String)
   end
 
+  describe 'Dish Selection' do
+
+    it 'will raise an error if dish name is not included' do
+      menu = Menu.new
+      expect(menu.add_items(0)).to raise_error "Invalid entry"
+    end
+
+
+    xit 'will raise an error if a dish is selected that is not on the menu' do
+      menu = Menu.new
+      expect(menu.add_items("The Prawn ", 3)).to eq 3
+    end
+  end
 
 end
