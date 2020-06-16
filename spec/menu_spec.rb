1a09c268d38becbd7d50f6a4a2809cc5ff360a48
Graham <grahamfalconer@icloud.com>
2020-06-16 16:37:47 +0100

    Added Dishes to Menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,4 +1,8 @@
 require_relative '../src/menu.rb'
 
 describe Menu do
+  it 'can show a list of dishes and prices' do
+    menu = Menu.new
+    expect(menu.show_menu).to be_a_kind_of(String)
+  end
 end
--- src/menu.rb
+++ src/menu.rb
@@ -1,2 +1,22 @@
 class Menu
+  attr_reader :dishes
+
+  def initialize
+    @dishes = {
+      "Prawn Nigiri" => 3,
+      "Salmon Nigiri" => 3,
+      "Tuna Hand Roll" => 5,
+      "Green Dragon Roll" => 8,
+      "Katsu Curry" => 8.50,
+      "Harmony Set" => 13,
+      "Katsu Bento" => 16
+    }
+  end
+
+  def show_menu()
+    @dishes.each do |dish, price|
+      return "The #{dish}   -   £#{price}"
+    end
+  end
+
 end
