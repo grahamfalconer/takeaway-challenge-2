eeec46ca3ace88dfd98ce5eafaed4ac2077b4f80
Graham <grahamfalconer@icloud.com>
2020-06-16 16:57:56 +0100

    undefined method items for Basket

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -4,6 +4,7 @@ describe Basket do
   it 'can recieve items' do
     basket = Basket.new
     item = ["The Prawn Nigiri",  3]
+    
     basket.items << item
     expect(basket.items.length).to eq 1
   end
