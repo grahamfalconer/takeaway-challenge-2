98eecc095591e4c0446a61e7e455c0b4e06934f0
Graham <grahamfalconer@icloud.com>
2020-06-17 17:27:36 +0100

    Siginign off for the day

--- spec/item_printer_spec.rb
+++ spec/item_printer_spec.rb
@@ -1,8 +1,13 @@
-require_relative '../src/item_printer.rb'
-
-describe ItemPrinter do
-  it 'Prints the item when prompted to' do
-    item_printer = ItemPrinter.new
-    expect(item_printer.print_items).to be_a_kind_of(String)
-  end
-end
+# require_relative '../src/item_printer.rb'
+#
+# describe ItemPrinter do
+#   it 'Prints the item when prompted to' do
+#     item_printer = ItemPrinter.new
+#     expect(item_printer.print_items).to be_a_kind_of(String)
+#   end
+#
+#   it 'calculates the items subtotal' do
+#     item_printer = ItemPrinter.new
+#     expect(item)
+#   end
+# end
--- src/item.rb
+++ src/item.rb
@@ -14,25 +14,4 @@ class Item
   def one_less
     @quantity -= 1
   end
-
 end
-
-item1 = Item.new("Prawn Nigiri", 3, 5)
-item2 = Item.new("Katsu", 3, 5)
-item3 = Item.new("Octopus", 3, 3)
-item4 = Item.new("Prawn Nigiri", 3, 5)
-item5 = Item.new("Katsu", 3, 5)
-item6 = Item.new("Octopus", 3, 3)
-item_array = [item1, item2, item3, item4, item5, item6]
-
-def item_printer(items)
-  total = []
-  items.each do |item|
-    total << item.price * item.quantity
-    puts "#{item.dish} x#{item.quantity} - £#{(item.quantity * item.price)}"
-  end
-  puts "----------------------"
-  return "Total: £#{total.inject(0){|sum,x| sum + x }}"
-end
-
-p item_printer(item_array)
